f8091e9f5dbb3042c313ee527f6dcdacf94b7f5b
Max <max@privatevoid.net>
2024-05-04 23:08:25 +0200

    dns: listen on dynamic ip:port, use different search domains for each interface

--- dns/server.go
+++ dns/server.go
@@ -17,26 +17,36 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-var domainSuffix = "hyprspace."
+func domainSuffix(config config.Config) string {
+	if config.Interface == "hyprspace" {
+		return "hyprspace."
+	} else {
+		return fmt.Sprintf("%s.hyprspace.", config.Interface)
+	}
+}
 
-func mkAliasRecord(alias string, p peer.ID) *dns.CNAME {
+func withDomainSuffix(config config.Config, str string) string {
+	return fmt.Sprintf("%s.%s", str, domainSuffix(config))
+}
+
+func mkAliasRecord(config config.Config, alias string, p peer.ID) *dns.CNAME {
 	cid, _ := peer.ToCid(p).StringOfBase(multibase.Base36)
 	return &dns.CNAME{
 		Hdr: dns.RR_Header{
-			Name:   fmt.Sprintf("%s.%s", alias, domainSuffix),
+			Name:   withDomainSuffix(config, alias),
 			Rrtype: dns.TypeCNAME,
 			Class:  dns.ClassINET,
 			Ttl:    0,
 		},
-		Target: fmt.Sprintf("%s.%s", cid, domainSuffix),
+		Target: withDomainSuffix(config, cid),
 	}
 }
 
-func mkIDRecord(p peer.ID, addr net.IP) *dns.A {
+func mkIDRecord(config config.Config, p peer.ID, addr net.IP) *dns.A {
 	cid, _ := peer.ToCid(p).StringOfBase(multibase.Base36)
 	return &dns.A{
 		Hdr: dns.RR_Header{
-			Name:   fmt.Sprintf("%s.%s", cid, domainSuffix),
+			Name:   withDomainSuffix(config, cid),
 			Rrtype: dns.TypeA,
 			Class:  dns.ClassINET,
 			Ttl:    86400,
@@ -68,20 +78,20 @@ func writeResponse(msg *dns.Msg, q dns.Question, p peer.ID, addr net.IP) {
 }
 
 func MagicDnsServer(ctx context.Context, config config.Config, node host.Host) {
-	dns.HandleFunc(domainSuffix, func(w dns.ResponseWriter, r *dns.Msg) {
+	dns.HandleFunc(domainSuffix(config), func(w dns.ResponseWriter, r *dns.Msg) {
 		m := new(dns.Msg)
 		m.SetReply(r)
 
 		for _, q := range r.Question {
 			switch q.Qtype {
 			case dns.TypeA:
-				if qpeer, err := peer.Decode(strings.TrimSuffix(q.Name, "."+domainSuffix)); err == nil {
+				if qpeer, err := peer.Decode(strings.TrimSuffix(q.Name, "."+domainSuffix(config))); err == nil {
 					if qpeer == node.ID() {
-						m.Answer = append(m.Answer, mkIDRecord(node.ID(), config.BuiltinAddr))
+						m.Answer = append(m.Answer, mkIDRecord(config, node.ID(), config.BuiltinAddr))
 					} else {
 						for _, p := range config.Peers {
 							if p.ID == qpeer {
-								m.Answer = append(m.Answer, mkIDRecord(p.ID, p.BuiltinAddr))
+								m.Answer = append(m.Answer, mkIDRecord(config, p.ID, p.BuiltinAddr))
 								break
 							}
 						}
@@ -92,14 +102,14 @@ func MagicDnsServer(ctx context.Context, config config.Config, node host.Host) {
 						fmt.Println("[!] [dns] " + err.Error())
 					}
 
-					qName := strings.ToLower(strings.TrimSuffix(q.Name, "."+domainSuffix))
+					qName := strings.ToLower(strings.TrimSuffix(q.Name, "."+domainSuffix(config)))
 
 					if qName == strings.ToLower(hostname) {
-						m.Answer = append(m.Answer, mkAliasRecord(qName, node.ID()))
-						m.Answer = append(m.Answer, mkIDRecord(node.ID(), config.BuiltinAddr))
+						m.Answer = append(m.Answer, mkAliasRecord(config, qName, node.ID()))
+						m.Answer = append(m.Answer, mkIDRecord(config, node.ID(), config.BuiltinAddr))
 					} else if p, found := config.PeerLookup.ByName[qName]; found {
-						m.Answer = append(m.Answer, mkAliasRecord(qName, p.ID))
-						m.Answer = append(m.Answer, mkIDRecord(p.ID, p.BuiltinAddr))
+						m.Answer = append(m.Answer, mkAliasRecord(config, qName, p.ID))
+						m.Answer = append(m.Answer, mkIDRecord(config, p.ID, p.BuiltinAddr))
 					}
 				}
 			}
@@ -108,13 +118,20 @@ func MagicDnsServer(ctx context.Context, config config.Config, node host.Host) {
 		w.WriteMsg(m)
 	})
 
+	dnsServerAddrBytes := []byte{127, 80, 01, 53}
+	var dnsServerPort uint16 = 5380
+	for i, b := range []byte(config.Interface) {
+		dnsServerAddrBytes[(i%3)+1] ^= b
+		dnsServerPort = (dnsServerPort+uint16(b))%40000 + 5000
+	}
+	dnsServerAddr := net.IP(dnsServerAddrBytes)
 	for _, netType := range []string{"tcp", "udp"} {
 		sv := &dns.Server{
-			Addr:      "127.80.1.53:5380",
+			Addr:      fmt.Sprintf("%s:%d", dnsServerAddr, dnsServerPort),
 			Net:       netType,
 			ReusePort: true,
 		}
-		fmt.Printf("[-] Starting DNS server on /ip4/127.80.1.53/%s/5380\n", sv.Net)
+		fmt.Printf("[-] Starting DNS server on /ip4/%s/%s/%d\n", dnsServerAddr, sv.Net, dnsServerPort)
 		go func(server *dns.Server) {
 			if err := server.ListenAndServe(); err != nil {
 				fmt.Printf("[!] DNS server error: %s, %s\n", server.Net, err.Error())
@@ -140,13 +157,13 @@ func MagicDnsServer(ctx context.Context, config config.Config, node host.Host) {
 		func() error {
 			return conn.SetLinkDNSEx(ctx, linkID, []resolved.LinkDNSEx{{
 				Family:  syscall.AF_INET,
-				Address: []byte{127, 80, 1, 53},
-				Port:    5380,
+				Address: dnsServerAddrBytes,
+				Port:    dnsServerPort,
 			}})
 		},
 		func() error {
 			return conn.SetLinkDomains(ctx, linkID, []resolved.LinkDomain{{
-				Domain:        domainSuffix,
+				Domain:        domainSuffix(config),
 				RoutingDomain: false,
 			}})
 		},

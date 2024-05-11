b693b6f0e0122cc2d99a0f8dd92c5c5cbbbe27a9
Max <max@privatevoid.net>
2024-05-11 20:09:58 +0200

    create proxies from multiaddrs

--- svc/proxy.go
+++ svc/proxy.go
@@ -2,11 +2,14 @@ package svc
 
 import (
 	"context"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"net"
 
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multiaddr"
 )
 
 type Proxy struct {
@@ -23,6 +26,42 @@ func (p Proxy) ServeFunc() func(net.Listener) error {
 			go p.Handle(conn)
 		}
 	}
+}
+
+func ProxyTo(ma multiaddr.Multiaddr) (Proxy, error) {
+	var proxy Proxy
+
+	var err error
+	var done bool
+
+	var ipAddr net.IP = net.IPv4(127, 0, 0, 1)
+	var tcpPort uint16
+
+	multiaddr.ForEach(ma, func(c multiaddr.Component) bool {
+		if done {
+			err = errors.New("trailing components in address")
+			return false
+		}
+		switch c.Protocol().Code {
+		case multiaddr.P_IP4:
+			fallthrough
+		case multiaddr.P_IP6:
+			ipAddr = net.IP(c.RawValue())
+			return true
+		case multiaddr.P_TCP:
+			tcpPort = binary.BigEndian.Uint16(c.RawValue())
+			proxy = TCPServiceProxy(net.TCPAddr{
+				IP:   ipAddr,
+				Port: int(tcpPort),
+			})
+			done = true
+		default:
+			err = errors.New("unsupported protocol: " + c.Protocol().Name)
+			return false
+		}
+		return false
+	})
+	return proxy, err
 }
 
 type RemoteServiceProxyStatus byte

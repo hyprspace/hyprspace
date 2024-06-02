b9be7646d26255267ee5688b24f32986ac5727cd
Max <max@privatevoid.net>
2024-06-02 20:38:41 +0200

    check that Nix and Go code is formatted correctly

--- dev/formatting.nix
+++ dev/formatting.nix
@@ -1,7 +1,29 @@
+{ lib, ... }:
+
 {
   perSystem =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     {
       formatter = pkgs.nixfmt-rfc-style;
+      checks =
+        let
+          inherit (lib.fileset) toSource fileFilter;
+          filesWithExtension =
+            ext:
+            toSource {
+              root = ../.;
+              fileset = fileFilter (file: file.hasExt ext) ../.;
+            };
+        in
+        {
+          format-nix = pkgs.runCommandNoCC "format-nix" { } ''
+            ${lib.getExe config.formatter} -c ${filesWithExtension "nix"}
+            touch $out
+          '';
+          format-go = pkgs.runCommandNoCC "format-go" { } ''
+            test "$(${pkgs.go}/bin/gofmt -l ${filesWithExtension "go"} | tee /dev/stderr | wc -l)" == 0
+            touch $out
+          '';
+        };
     };
 }

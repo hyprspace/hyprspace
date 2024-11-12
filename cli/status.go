4d8e966abc5089f028164fd563c468f2d382138b
Max <max@privatevoid.net>
2024-11-12 09:28:50 +0100

    cli: color p2p-circuit addresses gray in status output (#64)

--- cli/status.go
+++ cli/status.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/DataDrake/cli-ng/v2/cmd"
 	"github.com/hyprspace/hyprspace/rpc"
@@ -14,17 +15,29 @@ var Status = cmd.Sub{
 	Run:   StatusRun,
 }
 
+func colorMultiaddr(s string) string {
+	if strings.HasSuffix(s, "/p2p-circuit") || strings.Contains(s, "/p2p-circuit/p2p/") {
+		return fmt.Sprintf("%s%s%s", "\033[90m", s, "\033[0m")
+	} else {
+		return s
+	}
+}
+
 func StatusRun(r *cmd.Root, c *cmd.Sub) {
 	ifName := r.Flags.(*GlobalFlags).InterfaceName
 	if ifName == "" {
 		ifName = "hyprspace"
 	}
 
+	maybeColorMultiaddr := colorMultiaddr
+	if !isTTY() {
+		maybeColorMultiaddr = func(s string) string { return s }
+	}
 	status := rpc.Status(ifName)
 	fmt.Println("PeerID:", status.PeerID)
 	fmt.Println("Swarm peers:", status.SwarmPeersCurrent)
 	fmt.Printf("Connected VPN nodes: %d/%d\n", status.NetPeersCurrent, status.NetPeersMax)
-	printList(status.NetPeerAddrsCurrent)
+	printListF(status.NetPeerAddrsCurrent, maybeColorMultiaddr)
 	fmt.Println("Addresses:")
-	printList(status.ListenAddrs)
+	printListF(status.ListenAddrs, maybeColorMultiaddr)
 }
--- cli/util.go
+++ cli/util.go
@@ -1,9 +1,24 @@
 package cli
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/mattn/go-isatty"
+)
 
 func printList(strings []string) {
 	for _, ma := range strings {
 		fmt.Printf("    %s\n", ma)
 	}
+}
+
+func printListF(strings []string, handler func(string) string) {
+	for _, s := range strings {
+		fmt.Printf("    %s\n", handler(s))
+	}
+}
+
+func isTTY() bool {
+	return isatty.IsTerminal(os.Stdout.Fd())
 }

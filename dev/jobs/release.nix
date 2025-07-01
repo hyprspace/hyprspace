300330c5b394e96ffe3f823e26f155bfbb1a8416
Max <max@privatevoid.net>
2025-07-01 18:28:31 +0200

    auto-tag releases from current package version

--- dev/jobs/release.nix
+++ dev/jobs/release.nix
@@ -1,7 +1,15 @@
-{ lib, ... }:
+{
+  config,
+  lib,
+  withSystem,
+  ...
+}:
 
 {
   hercules-ci.github-releases = {
+    condition = { branch, ... }: branch == "master";
+    releaseTag =
+      _: "v${withSystem config.defaultEffectSystem ({ config, ... }: config.packages.hyprspace.version)}";
     filesPerSystem =
       { config, system, ... }:
       [

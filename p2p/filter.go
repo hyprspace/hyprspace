f0c30f0769db0c08bb50bd171110de14381cb691
Roland Urbano <urbano.roland@gmail.com>
2026-05-18 22:20:44 +0200

    Add filterPrivateAddresses option to block RFC1918/link-local dials (#101)
    * Add filterPrivateAddresses option to block RFC1918/link-local dials (#22)
    
    * Apply suggestion from @max-privatevoid
    
    Co-authored-by: Max <max@privatevoid.net>
    
    * Simplified config parsing
    
    Signed-off-by: Roland Urbano <urbano.roland@gmail.com>
    
    * NewRecursionGater: return connmgr.ConnectionGater
    
    * implement MultiGater
    
    * implement FilterGater
    
    * use MultiGater for private address filtering
    
    ---------
    
    Signed-off-by: Roland Urbano <urbano.roland@gmail.com>
    Co-authored-by: Max <max@privatevoid.net>

---
+++ p2p/filter.go
@@ -0,0 +1,45 @@
+package p2p
+
+import (
+	"net"
+
+	"github.com/libp2p/go-libp2p/core/connmgr"
+	"github.com/libp2p/go-libp2p/core/control"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multiaddr"
+)
+
+type FilterGater struct {
+	Filters *multiaddr.Filters
+}
+
+func NewFilterGater(networks ...net.IPNet) connmgr.ConnectionGater {
+	filters := multiaddr.NewFilters()
+	for _, network := range networks {
+		filters.AddFilter(network, multiaddr.ActionDeny)
+	}
+	return FilterGater{
+		Filters: multiaddr.NewFilters(),
+	}
+}
+
+func (f FilterGater) InterceptAccept(addrs network.ConnMultiaddrs) (allow bool) {
+	return !f.Filters.AddrBlocked(addrs.RemoteMultiaddr())
+}
+
+func (f FilterGater) InterceptAddrDial(_ peer.ID, addr multiaddr.Multiaddr) (allow bool) {
+	return !f.Filters.AddrBlocked(addr)
+}
+
+func (f FilterGater) InterceptPeerDial(peer.ID) (allow bool) {
+	return true
+}
+
+func (f FilterGater) InterceptSecured(_ network.Direction, _ peer.ID, addrs network.ConnMultiaddrs) (allow bool) {
+	return !f.Filters.AddrBlocked(addrs.RemoteMultiaddr())
+}
+
+func (f FilterGater) InterceptUpgraded(network.Conn) (allow bool, reason control.DisconnectReason) {
+	return true, 0
+}

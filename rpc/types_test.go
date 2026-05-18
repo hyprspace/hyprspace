e6f9e9628aa21c35f6baf4521e9eaff2a8797faa
Roland Urbano <roland.urbano@nts.eu>
2026-05-18 23:30:48 +0200

    test(rpc): add T19-T21 - StatusReply, PeersReply, Args type shape tests (zero values, populated)

--- rpc/types_test.go
+++ rpc/types_test.go
@@ -23,3 +23,70 @@ func TestT18_RouteActionConstants_AreDistinct(t *testing.T) {
 	assert.NotEqual(t, Add, Del)
 	assert.NotEqual(t, Show, Del)
 }
+
+func TestT19_StatusReply_ZeroValue(t *testing.T) {
+	reply := StatusReply{}
+	assert.Equal(t, "", reply.PeerID)
+	assert.Equal(t, 0, reply.SwarmPeersCurrent)
+	assert.Equal(t, 0, reply.NetPeersCurrent)
+	assert.Nil(t, reply.NetPeerAddrsCurrent)
+	assert.Equal(t, 0, reply.NetPeersMax)
+	assert.Nil(t, reply.ListenAddrs)
+}
+
+func TestT19_StatusReply_Populated(t *testing.T) {
+	reply := StatusReply{
+		PeerID:              "test-peer",
+		SwarmPeersCurrent:   3,
+		NetPeersCurrent:     2,
+		NetPeerAddrsCurrent: []string{"@node1 (10ms) /ip4/1.2.3.4/p2p/node1"},
+		NetPeersMax:         5,
+		ListenAddrs:         []string{"/ip4/0.0.0.0/tcp/4001"},
+	}
+
+	assert.Equal(t, "test-peer", reply.PeerID)
+	assert.Equal(t, 3, reply.SwarmPeersCurrent)
+	assert.Equal(t, 2, reply.NetPeersCurrent)
+	assert.Len(t, reply.NetPeerAddrsCurrent, 1)
+	assert.Equal(t, "@node1 (10ms) /ip4/1.2.3.4/p2p/node1", reply.NetPeerAddrsCurrent[0])
+	assert.Equal(t, 5, reply.NetPeersMax)
+	assert.Len(t, reply.ListenAddrs, 1)
+	assert.Equal(t, "/ip4/0.0.0.0/tcp/4001", reply.ListenAddrs[0])
+}
+
+func TestT20_PeersReply_ZeroValue(t *testing.T) {
+	reply := PeersReply{}
+	assert.Nil(t, reply.PeerAddrs)
+}
+
+func TestT20_PeersReply_Populated(t *testing.T) {
+	reply := PeersReply{
+		PeerAddrs: []string{"/ip4/1.2.3.4/p2p/abc", "/ip4/5.6.7.8/p2p/def"},
+	}
+
+	assert.Len(t, reply.PeerAddrs, 2)
+	assert.Contains(t, reply.PeerAddrs, "/ip4/1.2.3.4/p2p/abc")
+	assert.Contains(t, reply.PeerAddrs, "/ip4/5.6.7.8/p2p/def")
+}
+
+func TestT21_Args_ZeroValue(t *testing.T) {
+	args := Args{}
+	// Args struct has no fields - just verify zero value creation works
+}
+
+func TestT21_Args_Populated(t *testing.T) {
+	args := Args{
+		Name: "status",
+	}
+	assert.Equal(t, "status", args.Name)
+}
+
+func TestT21_Args_RouteArgs(t *testing.T) {
+	routeArgs := RouteArgs{
+		Action: Show,
+		Args:   []string{"show"},
+	}
+	assert.Equal(t, Show, routeArgs.Action)
+	assert.Len(t, routeArgs.Args, 1)
+	assert.Equal(t, "show", routeArgs.Args[0])
+}

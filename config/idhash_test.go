802b4fc98f3850ce687349d7094ee94cd92a4218
Roland Urbano <roland.urbano@nts.eu>
2026-05-22 22:27:29 +0200

    Removed useless tests.

--- config/idhash_test.go
+++ config/idhash_test.go
@@ -9,18 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_MkNetID_Deterministic(t *testing.T) {
-	pk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
-	require.NoError(t, err)
-	pid, err := peer.IDFromPrivateKey(pk)
-	require.NoError(t, err)
-
-	result1 := MkNetID(pid)
-	result2 := MkNetID(pid)
-
-	assert.Equal(t, result1, result2, "MkNetID should return the same result for the same peer ID")
-}
-
 func Test_MkNetID_AllZero(t *testing.T) {
 	zeroPeer := peer.ID([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 
@@ -41,26 +29,6 @@ func Test_MkNetID_AllFF(t *testing.T) {
 	expected := [4]byte{0x21, 0x52, 0x41, 0x10}
 	result := MkNetID(ffPeer)
 	assert.Equal(t, expected, result, "MkNetID with all-0xFF bytes should XOR correctly")
-}
-
-func Test_MkNetID_LongPeerID(t *testing.T) {
-	// Generate peers with different crypto types (varying lengths)
-	// No panic, no bounds issue regardless of peer ID length
-	testCases := []int{
-		crypto.Ed25519,
-		crypto.Secp256k1,
-	}
-
-	for _, kt := range testCases {
-		pk, _, err := crypto.GenerateKeyPair(kt, 256)
-		require.NoError(t, err)
-		pid, err := peer.IDFromPrivateKey(pk)
-		require.NoError(t, err)
-		assert.NotPanics(t, func() {
-			result := MkNetID(pid)
-			assert.Len(t, result, 4, "MkNetID should return 4 bytes for %s", pid)
-		}, "MkNetID should not panic on %s peer ID (type %d)", pid, kt)
-	}
 }
 
 func Test_MkNetID_CollisionResistance(t *testing.T) {
@@ -98,41 +66,12 @@ func Test_MkServiceID_SingleChar(t *testing.T) {
 	assert.Equal(t, expected, result, "Single-char service ID should equal empty")
 }
 
-func Test_MkServiceID_TwoChars(t *testing.T) {
-	result := MkServiceID("ab")
-
-	// "ab": id[0] ^= 'a' * 0 = 0, id[1] ^= 'b' * 1 = 'b'
-	// id[0] stays 0xff, id[1] = 0xfe ^ 0x62 = 0x9c
-	expected := [2]byte{0xff, 0x9c}
-	assert.Equal(t, expected, result, "MkServiceID(\"ab\") should be [0xff, 0x9c]")
-}
-
 func Test_MkServiceID_NonCommutative(t *testing.T) {
 	id1 := MkServiceID("ab")
 	id2 := MkServiceID("ba")
 
 	assert.NotEqual(t, id1, id2, "MkServiceID should be non-commutative")
 	assert.Equal(t, [2]byte{0xff, 0x9c}, id1, "MkServiceID(\"ab\") should be [0xff, 0x9c]")
-}
-
-func Test_MkServiceID_Deterministic(t *testing.T) {
-	result1 := MkServiceID("http-service")
-	result2 := MkServiceID("http-service")
-
-	assert.Equal(t, result1, result2, "MkServiceID should be deterministic")
-}
-
-func Test_MkBuiltinAddr4_Deterministic(t *testing.T) {
-	 pk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
-	require.NoError(t, err)
-	pid, err := peer.IDFromPrivateKey(pk)
-	require.NoError(t, err)
-
-	addr1 := mkBuiltinAddr4(pid)
-	addr2 := mkBuiltinAddr4(pid)
-
-	assert.Equal(t, addr1, addr2, "mkBuiltinAddr4 should be deterministic")
-	assert.NotNil(t, addr1)
 }
 
 func Test_MkBuiltinAddr4_AllZeros(t *testing.T) {
@@ -142,26 +81,6 @@ func Test_MkBuiltinAddr4_AllZeros(t *testing.T) {
 
 	expected := []byte{100, 64, 1, 2}
 	assert.Equal(t, expected, []byte(result.To4()), "mkBuiltinAddr4 with zero peer should return base address")
-}
-
-func Test_MkBuiltinAddr4_DifferentPeers(t *testing.T) {
-	ids := make([]peer.ID, 20)
-	for i := 0; i < 20; i++ {
-		pk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
-		require.NoError(t, err)
-		ids[i], err = peer.IDFromPrivateKey(pk)
-		require.NoError(t, err)
-	}
-
-	addrs := make(map[string]bool)
-	for _, pid := range ids {
-		addr := mkBuiltinAddr4(pid).To4()
-		addrStr := addr.String()
-		assert.False(t, addrs[addrStr], "mkBuiltinAddr4 collision for peer %s: %s", pid, addrStr)
-		addrs[addrStr] = true
-	}
-
-	assert.Equal(t, 20, len(addrs), "All 20 peers should have unique IPv4 addresses")
 }
 
 func Test_MkBuiltinAddr4_VaryingLengths(t *testing.T) {
@@ -198,35 +117,6 @@ func Test_MkBuiltinAddr4_StartsWith100_64(t *testing.T) {
 	}
 }
 
-func Test_MkBuiltinAddr6_Deterministic(t *testing.T) {
-	pk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
-	require.NoError(t, err)
-	pid, err := peer.IDFromPrivateKey(pk)
-	require.NoError(t, err)
-
-	addr1 := mkBuiltinAddr6(pid)
-	addr2 := mkBuiltinAddr6(pid)
-
-	assert.Equal(t, addr1, addr2, "mkBuiltinAddr6 should be deterministic")
-	assert.NotNil(t, addr1)
-	assert.Equal(t, 16, len(addr1.To16()), "Should return a valid 16-byte IPv6 address")
-}
-
-func Test_MkBuiltinAddr6_NetIDConsistency(t *testing.T) {
-	pk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
-	require.NoError(t, err)
-	pid, err := peer.IDFromPrivateKey(pk)
-	require.NoError(t, err)
-
-	ipv6 := mkBuiltinAddr6(pid)
-	netID := MkNetID(pid)
-
-	assert.Equal(t, netID[0], ipv6[12], "IPv6 byte 12 should match netID[0]")
-	assert.Equal(t, netID[1], ipv6[13], "IPv6 byte 13 should match netID[1]")
-	assert.Equal(t, netID[2], ipv6[14], "IPv6 byte 14 should match netID[2]")
-	assert.Equal(t, netID[3], ipv6[15], "IPv6 byte 15 should match netID[3]")
-}
-
 func Test_MkBuiltinAddr6_Prefix(t *testing.T) {
 	pk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
 	require.NoError(t, err)
@@ -257,18 +147,6 @@ func Test_MkBuiltinAddr6_DifferentPeers(t *testing.T) {
 	}
 
 	assert.Equal(t, 10, len(addrs), "All 10 peers should have unique IPv6 addresses")
-}
-
-func Test_MkServiceAddr6_Deterministic(t *testing.T) {
-	pk, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
-	require.NoError(t, err)
-	pid, err := peer.IDFromPrivateKey(pk)
-	require.NoError(t, err)
-
-	addr1 := MkServiceAddr6(pid, "http")
-	addr2 := MkServiceAddr6(pid, "http")
-
-	assert.Equal(t, addr1, addr2, "MkServiceAddr6 should be deterministic")
 }
 
 func Test_MkServiceAddr6_DifferentServices(t *testing.T) {

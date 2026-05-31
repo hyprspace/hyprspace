6fc30421a6b62a17c01c8289ffb4aaab220e3660
Max <max@privatevoid.net>
2026-05-31 17:21:13 +0200

    update vendorHash

--- package.nix
+++ package.nix
@@ -22,7 +22,7 @@ buildGoModule {
 
   env.CGO_ENABLED = "0";
 
-  vendorHash = "sha256-O604bzvz6QjDdM9hK4gya3vOjlki4ZaohY363mi3Of4=";
+  vendorHash = "sha256-mgRFDLZVF896PdItwk6zLgC5Btw3AOzdQDxiwopJlPE=";
 
   ldflags = [
     "-s"

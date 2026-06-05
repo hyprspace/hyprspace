b7acc862cb01adf519c41267d34579ac39c3b382
Max <max@privatevoid.net>
2026-06-05 23:38:12 +0200

    cli: factor out key generation

--- cli/util.go
+++ cli/util.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/mattn/go-isatty"
 )
 
@@ -15,4 +17,25 @@ func printListF(strings []string, handler func(string) string) {
 
 func isTTY() bool {
 	return isatty.IsTerminal(os.Stdout.Fd())
+}
+
+func GenerateKeyPair() ([]byte, peer.ID, error) {
+	var zeroID peer.ID
+
+	privKey, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
+	if err != nil {
+		return nil, zeroID, err
+	}
+
+	keyBytes, err := crypto.MarshalPrivateKey(privKey)
+	if err != nil {
+		return nil, zeroID, err
+	}
+
+	peerId, err := peer.IDFromPrivateKey(privKey)
+	if err != nil {
+		return nil, zeroID, err
+	}
+
+	return keyBytes, peerId, nil
 }
